e3219c3a5905f4e249d0f967110dfe04577d5194
Marcos Javier Gualco <marcosjaviergualco7@gmail.com>
2022-04-17 21:44:04 -0300

    Se continua el agregado de validaciones con excepciones

--- CapaUsuarios/FCrearActividad.cs
+++ CapaUsuarios/FCrearActividad.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CapaNegocio;
+using Excepciones;
 
 namespace CapaUsuarios
 {
@@ -25,16 +26,39 @@ namespace CapaUsuarios
 
         private void btnCrear_Click(object sender, EventArgs e)
         {
-            int unId = int.Parse(this.textBoxId.Text);
-            string unaDesc = this.textBoxDesc.Text;
-            DateTime unDiaYHora = this.dateTimePickerDiaYHora.Value;
-            float unCosto = float.Parse(this.textBoxCosto.Text);
-            int unaCantMax = int.Parse(this.textBoxCantMax.Text);
-            Profesor prof = (Profesor)listBoxProfesores.SelectedItem;
+            try
+            {
+                int unId = int.Parse(this.textBoxId.Text);
+                string unaDesc = this.textBoxDesc.Text;
+                DateTime unDiaYHora = this.dateTimePickerDiaYHora.Value;
+                float unCosto = float.Parse(this.textBoxCosto.Text);
+                int unaCantMax = int.Parse(this.textBoxCantMax.Text);
+                Profesor prof = (Profesor)listBoxProfesores.SelectedItem;
 
-            act = new Actividad(unId, unaDesc, unDiaYHora, unCosto, unaCantMax, prof);
+                act = new Actividad(unId, unaDesc, unDiaYHora, unCosto, unaCantMax, prof);
 
-            this.Close();
+                if( unaDesc.Length == 0)
+                {
+                    throw new DescripcionException();
+                    textBoxDesc.Focus();
+                }
+                if ( unCosto == 0 )
+                {
+                    throw new CostoException();
+                    textBoxCosto.Focus();
+                }
+
+                this.Close();
+            }
+            catch (DescripcionException ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBoxDesc.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
